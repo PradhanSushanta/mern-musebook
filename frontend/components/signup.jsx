455b6dfee5dbdeea29d17bef76e5b973ecd8259a
PradhanSushanta <pradhansushantakumar5@gmail.com>
2025-07-25 00:33:34 +0530

    render url

--- frontend/components/signup.jsx
+++ frontend/components/signup.jsx
@@ -33,7 +33,7 @@ const Signup = () => {
     }
 
     try {
-      const response = await axios.post("https://mern-musebook.onrender.com/register", formDta);
+      const response = await axios.post("https://mern-musebook.onrender.com/signup", formDta);
       alert("Registration successful! Redirecting to Login...");
       navigate("/login");
     } catch (error) {
